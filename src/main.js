3bb6fadf49d56fcf22c753a9c4d9b1c7ad9acc5d
@luccabasto <lcbeezus@outlook.com>
2024-02-02 19:28:26 -0300

    function timer in producion

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 
-const hourEl = document.getElementById("#hour")
-const minutesEl = document.getElementById("#minutes")
-const secondsEl = document.getElementById("#seconds")
+const hourEl = document.getElementById("hour")
+const minutesEl = document.getElementById("minutes")
+const secondsEl = document.getElementById("seconds")
 
 //btn's 
 const startBtn = document.getElementById("btn-start")
@@ -10,3 +10,52 @@ const continueBtn = document.getElementById("btn-continue")
 const resetBtn = document.getElementById("btn-reset")
 
 // #01Note
+let interval; 
+let hour = 0;
+let minutes = 0;
+let seconds = 0;
+let isPaused = false;
+
+//start timer
+startBtn.addEventListener("click", startTimer)
+
+function startTimer(){
+
+    interval = setInterval(() => {
+
+        if(!isPaused){
+
+            seconds += 1
+
+            if(seconds === 60){
+                minutes++;
+                seconds = 0;
+            }
+
+            if(minutes === 60){
+                hour++;
+                minutes = 0;
+            }
+
+            hourEl.textContent = formatTime(hour)
+            minutesEl.textContent = formatTime(minutes)
+            secondsEl.textContent = formatTime(seconds)
+
+        }
+    },1000);
+}
+
+// #02Note
+    //Format Timer
+
+function formatTime(time){
+    return time < 10 ? `0${time}`: time; 
+}
+
+
+
+//resume timer
+
+//pause timer
+
+//continue timer
