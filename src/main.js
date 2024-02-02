be3fbe0273e25fc5d8e44e57216cdae74c805f70
@luccabasto <lcbeezus@outlook.com>
2024-02-02 17:26:26 -0300

    funct timer in producion

---
+++ src/main.js
@@ -0,0 +1,12 @@
+
+const hourEl = document.getElementById("#hour")
+const minutesEl = document.getElementById("#minutes")
+const secondsEl = document.getElementById("#seconds")
+
+//btn's 
+const startBtn = document.getElementById("btn-start")
+const pauseBtn = document.getElementById("btn-pause")
+const continueBtn = document.getElementById("btn-continue")
+const resetBtn = document.getElementById("btn-reset")
+
+// #01Note
